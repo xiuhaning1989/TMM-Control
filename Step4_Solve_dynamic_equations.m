888d6e3ac8aac66f62fce0216be4df8a4b318e33
xiuhaning1989 <150053394+xiuhaning1989@users.noreply.github.com>
2023-11-13 12:29:50 -0500

    Add files via upload

--- Step4_Solve_dynamic_equations.m
+++ Step4_Solve_dynamic_equations.m
@@ -1,6 +1,6 @@
 clear all
 clc
-load Homogeneous_lattice_angles.mat
+load Homogeneous_lattice_angles_bistable_new.mat
 load Variables_names_m_by_n_lattice.mat
 load Coor_hexagon.mat
 % load Variables_names_unit_cell.mat
@@ -10,33 +10,29 @@ load Coor_hexagon.mat
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Blue Triangle
-global a_b; global b_b; global c_b;
 a_b=0.5;b_b=0.7;c_b=1;
-
-global psi_ab; global psi_bb; global psi_cb;
 psi_ab=acos((a_b^2-b_b^2-c_b^2)/(-2*b_b*c_b));
 psi_bb=acos((b_b^2-a_b^2-c_b^2)/(-2*a_b*c_b));
 psi_cb=acos((c_b^2-b_b^2-a_b^2)/(-2*b_b*a_b));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Red Triangle
-global a_r; global b_r; global c_r;
 a_r=0.4;b_r=0.8;c_r=1;
-
-global psi_ar; global psi_br; global psi_cr;
 psi_ar=acos((a_r^2-b_r^2-c_r^2)/(-2*b_r*c_r));
 psi_br=acos((b_r^2-a_r^2-c_r^2)/(-2*a_r*c_r));
 psi_cr=acos((c_r^2-b_r^2-a_r^2)/(-2*b_r*a_r));
 
-global l_s; l_s=(a_b+b_r)/2*0.95;
-
-global k_bond; global k_spring; 
-global eta_bond; global eta_spring; 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+l_s=(a_b+b_r)/2*0.95;
 k_bond=1e4; k_spring=100;
-eta_bond=100; eta_spring=20;
-
-global m_b; global m_r;
+eta_bond=30; eta_spring=20;
 m_b=0.1;m_r=0.1;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Lattice_configuration=[a_b b_b c_b psi_ab psi_bb psi_cb;
+                    a_r b_r c_r psi_ar psi_br psi_cr;
+                    m_b m_r k_bond k_spring eta_bond eta_spring;
+                    l_s 0 0 0 0 0];
 
 %%
 
@@ -82,29 +78,39 @@ else
 end
 f_U_name=[f_U_name;f_U_name1];
 end
-F_external=[0].*ones(length(f_U_name),1);
+F_external=[0.1].*ones(length(f_U_name),1);
 
 
-delta_t=1e-4; T=30;
+delta_t=1e-4; T=1;
 Time=0:delta_t:T;
 omega=2*pi;
 Force_ext=F_external.*exp(-1*(Time-1).^2).*cos(omega*Time);
 
-u=zeros(2*(6*n*m+2*n+2*m),1);
+% u=zeros(2*(6*n*m+2*n+2*m),length(Time));
+initialvals=zeros(2*(6*n*m+2*n+2*m),1);
 
-for i = 1:length(Time)-1
-    
-    k1 = equation_motion_unit_cell_dissipation(Time(i),u(:,i),n,m,alpha_matrix,theta_matrix,...
-        U_entire_name,U_fix_name,Force_ext(:,i),f_U_name);
-    k2 = equation_motion_unit_cell_dissipation(Time(i)+0.5*delta_t,u(:,i)+0.5*k1*delta_t,n,m,alpha_matrix,theta_matrix,...
-        U_entire_name,U_fix_name,Force_ext(:,i),f_U_name);    
-    k3 = equation_motion_unit_cell_dissipation(Time(i)+0.5*delta_t,u(:,i)+0.5*k2*delta_t,n,m,alpha_matrix,theta_matrix,...
-        U_entire_name,U_fix_name,Force_ext(:,i),f_U_name); 
-    k4 = equation_motion_unit_cell_dissipation(Time(i)+delta_t,u(:,i)+k3*delta_t,n,m,alpha_matrix,theta_matrix,...
-        U_entire_name,U_fix_name,Force_ext(:,i),f_U_name); 
-    u(:,i+1) = u(:,i)+((k1+2*k2+2*k3+k4)/6)*delta_t;  
-end
+tic;
+options2 = odeset('RelTol',1e-8,'AbsTol',1e-8.*ones(1,2*(6*n*m+2*n+2*m)));
+[t, U] = ode45(@(t,u) equation_motion_unit_cell_dissipation(t,u,n,m,...
+    alpha_matrix,theta_matrix,Lattice_configuration,U_entire_name,U_fix_name,...
+    Time,Force_ext,f_U_name),0:delta_t:T, initialvals, options2);
+U=U';
 
+
+% for i = 1:length(Time)-1
+%     
+%     k1 = equation_motion_unit_cell_dissipation(Time(i),u(:,i),n,m,alpha_matrix,theta_matrix,Lattice_configuration,...
+%         U_entire_name,U_fix_name,Force_ext(:,i),f_U_name);
+%     k2 = equation_motion_unit_cell_dissipation(Time(i)+0.5*delta_t,u(:,i)+0.5*k1*delta_t,n,m,alpha_matrix,theta_matrix,...
+%         Lattice_configuration,U_entire_name,U_fix_name,Force_ext(:,i),f_U_name);    
+%     k3 = equation_motion_unit_cell_dissipation(Time(i)+0.5*delta_t,u(:,i)+0.5*k2*delta_t,n,m,alpha_matrix,theta_matrix,...
+%         Lattice_configuration,U_entire_name,U_fix_name,Force_ext(:,i),f_U_name); 
+%     k4 = equation_motion_unit_cell_dissipation(Time(i)+delta_t,u(:,i)+k3*delta_t,n,m,alpha_matrix,theta_matrix,...
+%         Lattice_configuration,U_entire_name,U_fix_name,Force_ext(:,i),f_U_name); 
+%     u(:,i+1) = u(:,i)+((k1+2*k2+2*k3+k4)/6)*delta_t;  
+% end
+elapsedTime = toc;
+fprintf('Elapsed time: %.4f seconds\n', elapsedTime);
 
 save('Dynamic_2_1_k2=100_eta2=20_f=0.mat')
 
